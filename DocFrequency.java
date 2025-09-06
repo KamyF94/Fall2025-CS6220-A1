502dd6d975966b8bd76af952859cedcef06a1bed
KamyF94 <165164534+KamyF94@users.noreply.github.com>
2025-09-05 23:48:05 -0400

    Add files via upload

---
+++ DocFrequency.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.util.HashSet;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class DocFrequency {
+
+    public static class Map extends Mapper<Object, Text, Text, Text> {
+        private Text word = new Text();
+        private Text filename = new Text();
+
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String fileNameStr = ((FileSplit) context.getInputSplit()).getPath().getName();
+            filename.set(fileNameStr);
+
+            String[] tokens = value.toString().split("\\W+");
+            HashSet<String> seen = new HashSet<>();
+            for (String token : tokens) {
+                if (token.length() > 0 && !seen.contains(token)) {
+                    word.set(token);
+                    context.write(word, filename);
+                    seen.add(token);
+                }
+            }
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, Text, Text, IntWritable> {
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+            HashSet<String> uniqueFiles = new HashSet<>();
+            for (Text val : values) {
+                uniqueFiles.add(val.toString());
+            }
+            context.write(key, new IntWritable(uniqueFiles.size()));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "doc frequency");
+        job.setJarByClass(DocFrequency.class);
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
